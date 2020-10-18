b223903501c8acae233ee8635bff8e3a7d4d3740
integriz <integriz@gmail.com>
2020-10-18 10:05:23 -0300

    first commit

---
+++ src/main/java/com/mmarques/blankspringbootjsp/BlankSpringBootJspApplication.java
@@ -0,0 +1,12 @@
+package com.mmarques.blankspringbootjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlankSpringBootJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlankSpringBootJspApplication.class, args);
+	}
+}
---
+++ src/main/java/com/mmarques/blankspringbootjsp/controller/TesteController.java
@@ -0,0 +1,13 @@
+package com.mmarques.blankspringbootjsp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TesteController {
+	
+	@GetMapping("/")
+	public String teste() {
+		return "pagina-teste";
+	}
+}
